6690068a551209b93901ee6f9f92540e3aab2ca6
Partow Roshani <roshanipartow@gmail.com>
2025-10-05 20:58:07 +0330

    Update firebase.py

---
+++ firebase.py
@@ -0,0 +1,10 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+cred = credentials.Certificate(os.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
+firebase_admin.initialize_app(cred)
+db = firestore.client()
